abee0c16d3335cfbf09a9c0866f5f67b01349d94
Petouha <rachidgames2.rg@gmail.com>
2024-06-03 17:27:14 +0200

    added signal delete

--- Scenes/Domino/DominoPalette/DominoPaletteRose.gd
+++ Scenes/Domino/DominoPalette/DominoPaletteRose.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
 signal click_domino_rose
-
+signal supp_domino(id)
 
 func _on_DominoPaletteRose_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.is_pressed():
 		emit_signal("click_domino_rose")
+	if event is InputEventMouseButton and event.button_index == BUTTON_RIGHT and event.is_pressed():
+		emit_signal("supp_domino",get_index())
--- Scenes/Domino/DominoPalette/DominoPaletteRouge.gd
+++ Scenes/Domino/DominoPalette/DominoPaletteRouge.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
 signal click_domino_rouge
-
+signal supp_domino(id)
 
 func _on_DominoPaletteRouge_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.is_pressed():
 		emit_signal("click_domino_rouge")
+	if event is InputEventMouseButton and event.button_index == BUTTON_RIGHT and event.is_pressed():
+		emit_signal("supp_domino",get_index())
